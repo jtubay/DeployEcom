0c3c7964a98bd14674f0607997db68f7d73ccd36
Jean Tubay <jean.tubay.92@gmail.com>
2020-05-24 21:15:06 -0400

    testing

---
+++ src/components/Cart/New.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+export class New extends Component {
+    render() {
+        return (
+            <div>
+                <h1>hello world</h1>
+                
+            </div>
+        )
+    }
+}
+
+
+
