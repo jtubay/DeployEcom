dd94ab3e19bb8d74e31c7cf37452a0c3e8c9f48a
Jean Tubay <jean.tubay.92@gmail.com>
2020-05-27 13:37:29 -0400

    fixing issues

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,67 +5,6 @@ import Logo from './nurhan.PNG'
 
 export default function Footer() {
     return (
-
-        // <FooterContainer  classNameName="main-footer">
-        // <div classNameName="footer-middle">
-        //     <div classNameName="container">
-        //         <div classNameName="row">
-        //             <div classNameName="col-md-4 col-sm-6 mb-1" >
-        //             <h2 classNameName="company mb-3">Nurhan Accesories</h2>
-        //                 <img src={Logo} classNameName="imgLogo" alt=""/>
-                        
-        //             </div>
-        //             {/* <div classNameName="col-md-3 col-sm-6">
-        //                 <h4>Lorem ipsum</h4>
-        //                 <ul classNameName="list-unstyled">
-        //                     <li>lorem upsim</li>
-        //                     <li>lorem upsim</li>
-        //                     <li>lorem upsim</li>
-        //                     <li>lorem upsim</li>  
-        //                 </ul>
-        //             </div> */}
-        //             <div classNameName="col-md-4 col-sm-6 mb-1">
-                        
-                        
-                       
-        //             <div classNameName="mb-1 mt-5 mb-3">
-        //                  <i classNameName="fa fa-map-marker"><span>  123 Main St</span></i>
-  
-        //              </div>
-     
-        //              <div classNameName="mb-3">
-        //                  <i classNameName="fa fa-phone"><span>  +1.555.555.5555</span></i>
-                         
-        //              </div>
-     
-        //              <div>
-        //                  <i classNameName="fa fa-envelope"> <a href="mailto:support@company.com" classNameName="email">  support@company.com</a></i>
-
-        //              </div>
-
-
-        //             </div>
-        //             <div classNameName="col-md-4 col-sm-6 mb-1 text-right">
-        //                 <h2 classNameName="contactMe">Contact Information</h2>
-        //                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, illum vero explicabo atque optio libero, consequuntur cupiditate, voluptate iure reprehenderit molestias dicta hic!</p>
-        //                 <i classNameName="fab fa-2x fa-facebook-square social"></i>
-        //              <i classNameName="fab fa-2x fa-instagram social"></i>
-        //              <i classNameName="fab fa-2x fa-twitter-square social"></i>
-        //             </div>
-
-        //         </div>
-        //         {/* footer copy */}
-        //         <div classNameName="footer-bottom">
- 
-        //                     <p classNameName="text-xs-center text-center">
-        //                         &copy;{new Date().getFullYear()} <span classNameName="compFoot">Nurhan Accessories</span> - All Rights Reserved
-        //                     </p>
-                        
-       
-        //         </div>
-        //     </div>
-        // </div>
-        // </FooterContainer >
         <FooterContainer>
             <footer className="footer">
                 <div className="container">
@@ -119,11 +58,6 @@ export default function Footer() {
                 </div>
 
 
-                {/* <div classNameName="footer-bottom">
-                     <p classNameName="text-xs-center text-center">
-                        &copy;{new Date().getFullYear()} <span classNameName="compFoot">Nurhan Accessories</span> - All Rights Reserved
-                    </p>
-                </div> */}
 
 
             </footer>
